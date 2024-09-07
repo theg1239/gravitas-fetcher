f75f9c3349b098b63decc4f570d1f5a109fb7db1
theg <52027622+theg1239@users.noreply.github.com>
2024-09-08 01:52:25 +0530

    update postbuild.bash

--- postbuild.bash
+++ postbuild.bash
@@ -1,6 +1,16 @@
+#!/bin/bash
+
 echo -e "Storing Puppeteer executable in cache\n"
 
-rm -rf ./.cache
-mkdir ./.cache
+mkdir -p /app/.cache/puppeteer
 
-mv /app/.cache/puppeteer ./.cache
+rm -rf ./.cache
+
+mkdir -p ./.cache
+
+if [ -d "/app/.cache/puppeteer" ]; then
+    echo "Moving Puppeteer cache to local cache directory"
+    mv /app/.cache/puppeteer ./.cache/
+else
+    echo "No Puppeteer cache found in /app/.cache/puppeteer"
+fi
