44b6d616050a1e27b9f0b811f74bd2573781e905
theg <52027622+theg1239@users.noreply.github.com>
2024-09-08 12:48:07 +0530

    fix

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.use(cors({
     origin: '*',
